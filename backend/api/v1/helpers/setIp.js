c5a8e6f9e131b0ff2f8c4424381325f75f618754
tommy gingras <tommy@studiowebux.com>
2019-10-28 19:28:03 -0400

    fix error with refresh token

--- backend/api/v1/helpers/setIp.js
+++ backend/api/v1/helpers/setIp.js
@@ -1,10 +1,11 @@
+const Webux = require("webux-app");
+
 /**
  * Return the IP of the client
  * @private
  * @param {Object} req Express req object
  */
 function setIp(req) {
-
   Webux.log.verbose("HELPER | Set IP Called");
   return (
     req.headers["x-forwarded-for"] ||
